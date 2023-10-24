0326dea8ec5a3047e27bbc20703d5ef646370753
Andrew Theiss <andrew.theiss@gmail.com>
2023-10-23 20:05:45 -0700

    Revert "Add scripts for database game example"
    This reverts commit 45b6353fee554a9e4aef02e48a9c180589be4aeb.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,5 @@
-
-
 class Main {
     constructor() {
-        this.someVariable = [];
         this.pageViewsKey = 'pageViewsCount';
         this.initializeCounter();
         this.displayCount();
@@ -23,51 +20,9 @@ class Main {
     displayCount() {
         this.incrementCount();
         // Update count in div id count
-        //document.getElementById('count').innerHTML = 'You have visited this page ' + localStorage.getItem(this.pageViewsKey)  + ' times.'
-    }
-
-    clearCount() {
-        localStorage.removeItem(this.pageViewsKey);
-        this.displayCount();
+        document.getElementById('count').innerHTML = 'You have visited this page ' + localStorage.getItem(this.pageViewsKey)  + ' times.'
     }
 }
 
 // Note that we construct the class here, but we don't need to assign it to a variable.
-new Main();
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('click', function(event) {
-    const container = document.getElementById('particle-container');
-
-    for (let i = 0; i < 50; i++) {  /* More particles */
-        const particle = document.createElement('div');
-        particle.classList.add('particle');
-
-        // Position particle at mouse location
-        particle.style.left = (event.clientX - 5) + 'px';  /* Adjusted for increased size */
-        particle.style.top = (event.clientY - 5) + 'px';   /* Adjusted for increased size */
-
-        // Random direction for each particle
-        const angle = Math.random() * 2 * Math.PI;
-        const distance = Math.random() * 150;  /* Particles travel farther */
-
-        // Calculate position based on the angle and distance
-        particle.style.transform = 'translate(' + Math.cos(angle) * distance + 'px, ' + Math.sin(angle) * distance + 'px)';
-
-        container.appendChild(particle);
-
-        // Remove particle after animation complete to prevent DOM clutter
-        setTimeout(function() {
-            particle.remove();
-        }, 1500);  /* Adjusted for longer animation duration */
-    }
-});
+document.mainClass = new Main();
